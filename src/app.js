0c83760ccccd69e1b8647ecec7cd932ceda099e5
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-02-24 11:02:01 -0500

    import Switch

--- src/app.js
+++ src/app.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const ExpenseDashboardPage = () => {
+    return (
+        <div>This is from my dashboard component</div>
+    )
+}
 
+const AddExpensePage = () => {
+    return (
+        <div>This is from my add expense component</div>
+    )
+}
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+const EditExpensePage = () => {
+    return (
+        <div>This is from my edit expense page component</div>
+    )
+}
+const HelpPage = () => {
+    return (
+        <div>This is from my help page component</div>
+    )
+}
+
+const NotFoundPage = () => {
+    return (
+        <div>404!</div>
+    )
+}
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route  path='/'        component={ExpenseDashboardPage} exact={true}   />
+            <Route  path='/create'  component={AddExpensePage}                      />
+            <Route  path='/edit'    component={EditExpensePage}                     />
+            <Route  path='/help'    component={HelpPage}                            />
+            <Route  component={NotFoundPage}                                        />
+        </Switch> 
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
