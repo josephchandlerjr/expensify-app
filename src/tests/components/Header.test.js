ae915a201ffb48f9522be3d35ea86355c1f2e944
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-03-11 12:40:29 -0400

    fix package.json

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -3,6 +3,7 @@ import ReactShallowRenderer from 'react-test-renderer/shallow'
 import Header from '../../components/Header'
 import { shallow } from 'enzyme'
 
+
 test('should render header correctly', () => {
     const wrapper = shallow(<Header />)
     expect(wrapper).toMatchSnapshot()
